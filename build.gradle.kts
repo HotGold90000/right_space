0c1e5295f326c6cf70d06ce10682a282afa78a0c
liumiao36 <liumiao36@xdf.cn>
2025-09-12 00:13:41 +0800

    fix GitHub Actions 步骤里找不到 UpdateJson.kt 文件和 gson-2.10.1.jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,3 @@
-//plugins {
-//    kotlin("jvm") version "1.9.23"
-//    application
-//}
-//
-//repositories {
-//    mavenCentral()
-//}
-//
-//dependencies {
-//    implementation("com.google.code.gson:gson:2.10.1")
-//}
-//
-//application {
-//    // 指定入口点
-//    mainClass.set("UpdateJsonKt")
-//}
-
 plugins {
     kotlin("jvm") version "1.9.0"
 }
